c6fe6751e921b3709673f7c19999830a5e3f2efa
Stalinjoy2001 <stalinjoy005@gmail.com>
2025-03-29 11:22:41 -0400

    Functions

---
+++ WeatherApp/WeatherApp/Weather.swift
@@ -0,0 +1,16 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Stalin on 29/03/25.
+//
+
+import Foundation
+
+struct WeatherData {
+    let city: String
+    let tempC: Double
+    let tempF: Double
+    let condition: String
+    let conditionCode: Int
+}
