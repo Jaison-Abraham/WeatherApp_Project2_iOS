127fea851f4d35271567f7a39ddcbc71210fea13
Jaison-Abraham <jaisonabraham8606@gmail.com>
2025-03-29 02:03:46 -0400

    initial commit

---
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by JAISON ABRAHAM on 2025-03-29.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
